2e05c272890b0ebf1a62f4940d0590255d7d0178
prateek-10 <prateeksibu333@gmail.com>
2024-07-04 05:05:43 +0530

    Navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,43 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        // 'caftarea':"url('https://ik.imagekit.io/cmef8hxb6/3_WA1kRjsQi.jpg?updatedAt=1679424300495')",
-        'logo': "url('/NIT_Rourkela_Colour_Logo.svg.png')",
-        // 'circles':"url('https://4kwallpapers.com/images/wallpapers/google-circles-multicolor-colorful-white-background-5k-8k-2732x2732-5352.jpg')",
-      }
-    }
+        logo: "url('/NIT_Rourkela_Colour_Logo.svg.png')",
+      },
+      backdropBlur: {
+        7: "7px",
+      },
+      animation: {
+        "text-logo-animate1": "textLogoAnimate1 12s ease-in-out infinite",
+        "text-logo-animate2": "textLogoAnimate2 12s ease-in-out infinite",
+        "text-logo-animate3": "textLogoAnimate3 12s ease-in-out infinite",
+      },
+      keyframes: {
+        textLogoAnimate1: {
+          "0%, 22%, 96%, 100%": { opacity: "1", transform: "translateY(0)" },
+          "26%": { opacity: "0", transform: "translateY(30px)" },
+          "32%, 59%, 63%, 67%, 92%": {
+            opacity: "0",
+            transform: "translateY(-30px)",
+          },
+        },
+        textLogoAnimate2: {
+          "0%, 22%, 67%, 92%, 96%, 100%": {
+            opacity: "0",
+            transform: "translateY(-30px)",
+          },
+          "63%": { opacity: "0", transform: "translateY(30px)" },
+          "26%, 32%, 59%": { opacity: "1", transform: "translateY(0px)" },
+        },
+        textLogoAnimate3: {
+          "0%, 22%, 26%, 32%, 59%, 100%": {
+            opacity: "0",
+            transform: "translateY(-30px)",
+          },
+          "63%, 67%, 92%": { opacity: "1", transform: "translateY(0)" },
+          "96%": { opacity: "0", transform: "translateY(30px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
